1591899bfc9594190dc052c5d9d579c17a70bb6c
Owais Badat <owaisbadat@gmail.com>
2018-09-16 13:57:52 -0500

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === ) {
+      myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
+var myButton = document.querySelector("button")
+var myHeading = document.querySelector("h1")
